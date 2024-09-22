759882fa39b5031e30281c874f9b85c84f1eef82
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-23 02:36:07 +0600

    actioned on two main button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log('connected index.js');
+document.getElementById('donationButton').addEventListener('click', function(){
+   
+    document.getElementById('historyContainer').classList.add('hidden');
+    document.getElementById('donationContainer').classList.remove('hidden');
+    document.getElementById('donationButton').classList.add('bg-primary');
+    document.getElementById('historyButton').classList.remove('bg-primary');
+   
+})
+
+document.getElementById('historyButton').addEventListener('click', function(){
+   
+    document.getElementById('donationContainer').classList.add('hidden');
+    document.getElementById('historyContainer').classList.remove('hidden');
+    document.getElementById('historyButton').classList.add('bg-primary');
+    document.getElementById('donationButton').classList.remove('bg-primary');
+})
