eee5be5c7a39b37dc6abfe55bdcc9ed930c0d6ab
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-23 07:20:26 +0600

    completed for noakhali

--- js/noakhali.js
+++ js/noakhali.js
@@ -8,11 +8,12 @@ document.getElementById('noakhaliDonationButton').addEventListener('click', func
     let input;
     input= getInputValueById('noakhaliDonationAmount');
     let x=inputFieldValidation(input);
+    let acoountBalance= getInnerTextById('acoountBalance');
     if(input.length===0){
         alert('Please Write some amount');
     }
     
-    else if(x===false){
+    else if(x===false || x>acoountBalance || x<0){
       showAlert();
     }
     else 
@@ -24,10 +25,34 @@ document.getElementById('noakhaliDonationButton').addEventListener('click', func
 
         let noakhaliDonationBalance = getInnerTextById('noakhaliDonationBalance');
         noakhaliDonationBalance +=x;
-        // alert(noakhaliDonationBalance);
         document.getElementById('noakhaliDonationBalance').innerText=noakhaliDonationBalance;
 
-     }
+        
+        acoountBalance -=x;
+        document.getElementById('acoountBalance').innerText=acoountBalance;
+        
+        let historyContainer= document.getElementById('historyContainer');
+        historyContainer.classList.add('space-y-4');
+        let newDiv = document.createElement('div');
+        // border border-[rgba(17, 17, 17, 0.1)] rounded-2xl p-8
+         newDiv.classList.add('border','border-[#1111111a]','rounded-2xl','p-8');
+        let h3 = document.createElement('h3');
+        h3.classList.add('text-md', 'font-bold');
+        h3.innerHTML=`${x}Taka is Donated for Donate for Flood at Noakhali, Bangladesh.
+        `;
+        let p= document.createElement('p');
+        p.classList.add('text-[#111111b3]');
+        p.innerHTML=`
+        Date: ${new Date()}
+        `;
+        newDiv.append(h3);
+        newDiv.append(p);
+        
+        historyContainer.append(newDiv);
+        console.log(historyContainer);
+
+    
+    }
 
 })
 
