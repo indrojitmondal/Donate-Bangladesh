c15e2d84407cedfa450eb0d48e29b1a3be3070b5
indroji-mondal <indro.cse.bu@gmail.com>
2024-09-23 07:55:19 +0600

    complete for quota movement Injuried

--- js/quotaMovement.js
+++ js/quotaMovement.js
@@ -1,1 +1,55 @@
 console.log('connected quotaMovement.js');
+
+document.getElementById('quotaDonationButton').addEventListener('click', function(event){
+    event.preventDefault();
+   //  alert('clicked feni');
+   let input;
+   input= getInputValueById('quotaDonationAmount');
+   let x=inputFieldValidation(input);
+   let acoountBalance= getInnerTextById('acoountBalance');
+   if(input.length===0){
+       alert('Please Write some amount');
+   }
+   
+   else if(x===false || x>acoountBalance || x<0){
+     showAlert();
+   }
+   else 
+    {
+       // Okay....
+       document.getElementById('my_modal').showModal();
+       document.getElementById('quotaDonationAmount').value='';
+       console.log(x);
+
+       let quotaDonationBalance = getInnerTextById('quotaDonationBalance');
+       quotaDonationBalance +=x;
+       document.getElementById('quotaDonationBalance').innerText=quotaDonationBalance;
+
+       
+       acoountBalance -=x;
+       document.getElementById('acoountBalance').innerText=acoountBalance;
+       
+       let historyContainer= document.getElementById('historyContainer');
+       historyContainer.classList.add('space-y-4');
+       let newDiv = document.createElement('div');
+       // border border-[rgba(17, 17, 17, 0.1)] rounded-2xl p-8
+        newDiv.classList.add('border','border-[#1111111a]','rounded-2xl','p-8');
+       let h3 = document.createElement('h3');
+       h3.classList.add('text-md', 'font-bold');
+       h3.innerHTML=`${x} Taka is Donated for Donate for Aid for Injured in the Quota Movement.
+       `;
+       let p= document.createElement('p');
+       p.classList.add('text-[#111111b3]');
+       p.innerHTML=`
+       Date: ${new Date()}
+       `;
+       newDiv.append(h3);
+       newDiv.append(p);
+       
+       historyContainer.append(newDiv);
+       console.log(historyContainer);
+
+   
+   }
+
+})
